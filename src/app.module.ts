783891d8ce3da69c873955e3a58620d2bf051197
hoangadamo <hoang.caoviet@adamosoft.com>
2024-10-15 22:00:33 +0700

    update modules-structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 
 import { PostgresqlModule } from './database';
-import { UsersModule } from './modules';
+import { AuthModule, RanksModule, RewardsModule, StoresModule, UsersModule } from './modules';
 import { AppController } from './app.controller';
-import { AuthModule } from './modules/auth/auth.module';
-import { RanksModule } from './modules/ranks/ranks.module';
-import { StoresModule } from './modules/stores/stores.module';
-import { RewardsModule } from './modules/rewards';
 
 @Module({
   imports: [
