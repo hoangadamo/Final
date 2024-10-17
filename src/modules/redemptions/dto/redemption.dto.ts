ed5a56cc91a64edb71c2b3e5099d8c6d47630dd0
hoangadamo <hoang.caoviet@adamosoft.com>
2024-10-17 15:57:14 +0700

    redemption-swagger

--- src/modules/redemptions/dto/redemption.dto.ts
+++ src/modules/redemptions/dto/redemption.dto.ts
@@ -1,12 +1,22 @@
-import { Type } from 'class-transformer';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsOptional, Max, Min } from 'class-validator';
 import { PaginationDto } from 'src/constants/dto';
 
 export class CreateRedemptionDto {
+  @ApiProperty({
+    type: Number,
+    description: 'rewardId',
+    example: 1,
+  })
   @IsNotEmpty()
   @IsNumber()
   rewardId: number;
 
+  @ApiProperty({
+    type: Number,
+    description: 'quantity',
+    example: 3,
+  })
   @IsNotEmpty()
   @IsNumber()
   @Min(1)
@@ -15,9 +25,19 @@ export class CreateRedemptionDto {
 }
 
 export class GetListRedemptionsDto extends PaginationDto {
+  @ApiPropertyOptional({
+    type: String,
+    description: 'reward name',
+    example: 'Sample',
+  })
   @IsOptional()
   rewardName?: string;
 
+  @ApiPropertyOptional({
+    type: Number,
+    description: 'filter by user id',
+    example: 1,
+  })
   @IsOptional()
   userId?: number;
 }
